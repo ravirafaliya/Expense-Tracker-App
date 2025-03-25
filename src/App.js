b6371fe2c524f945bd7d444abb248c415a9fb0da
Ravi Rafaliya <ravirr6997@gmail.com>
2025-03-25 14:29:00 +0530

    add: add transaction

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import Dashboard from "./pages/Dashboard"
 import Transaction from "./pages/Transaction"
 import Report from "./pages/Report"
 import Navbar from "./components/Navbar"
+import Notfound from "../src/pages/NotFound"
+import AddTransaction from "./pages/AddTransaction"
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 
@@ -13,11 +15,12 @@ function App(){
         <div>
         <Navbar/>
           <Routes>
-            <Route path="/" element={<Dashboard />} />
+            <Route path="/" element={<AddTransaction />} />
             <Route path="/transaction" element={<Transaction />} />
             <Route path="/reports" element={<Report />} />
+            <Route path="*" element={<Notfound />} />
           </Routes>
-          <Link to={"/transaction"}>Navigate</Link>
+          
         </div>
       </BrowserRouter>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles/Navbar.css";
+import { useEffect } from "react";
 
 function Navbar() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log(location);
+  },[location]);
+
   return (
     <nav className="navbar">
       <h1 className="logo">Expense Tracker</h1>
       <ul className="nav-links">
-        <li>
+        <li className={location.pathname === "/" ? "active" : ""}>
           <Link to={"/"}>Dashboard</Link>
         </li>
-        <li>
+        <li className={location.pathname === "/transaction" ? "active" : ""}>
           <Link to={"/transaction"}>Transaction</Link>
         </li>
-        <li>
+        <li className={location.pathname === "/reports" ? "active" : ""}>
           <Link to={"/reports"}>Reports</Link>
         </li>
         <li>
